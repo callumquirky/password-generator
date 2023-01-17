27f0950bc3991ff4642638dd532ff46e1d5bdc73
Callum Quirk <callumbquirk@gmail.com>
2023-01-17 15:58:46 +0000

    added code for generatePasword function

--- script.js
+++ script.js
@@ -88,19 +88,49 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+let hasSepecialCharacters = true;
+let hasNumericCharacters = true;
+let hasLowerCasedCharacters = true;
+let hasUpperCasedCharacters = true;
+let passwordLength = 0;
+let passwordLengthIsInteger = true;
+
 // Function to prompt user for password options
 function getPasswordOptions() {
-
+  while (passwordLength < 10, passwordLengthIsInteger == false) {
+		passwordLength = prompt ("Please specify password length between 8 and 164 characters");
+		passwordLengthIsInteger = Number.isInteger(passwordLength)
+	}
+	hasSpecialCharacters = confirm("Do you want special characters in your password?");
+	hasNumericCharacters = confirm("Do you want numeric characters in your password?");
+	hasLowerCasedCharacters = confirm("Do you want lower cased characters in your password?");
+	hasUpperCasedCharacters = confirm("Do you want upper characters in your password?");
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  Math.floor(Math.random() * arr.length);
+  return;
 }
 
 // Function to generate password with user input
 function generatePassword() {
-
+  let generatedPassword = ""
+  for (let i=0; i < passwordLength; i++){
+    if (hasSpecialCharacters) == true {
+    generatedPassword =+ getRandom(specialCharacters);
+    }
+    if (hasNumericCharacters) == true {
+    generatedPassword =+ getRandom(numericCharacters); 
+    }
+    if (haslowerCasedCharacters) == true {
+    generatedPassword =+ getRandom(lowerCasedCharacters);
+    }
+    if (hasUpperCasedCharacters) == true {
+    generatedPassword =+ getRandom(lowerCasedCharacters);
+    }
+  return generatedPassword;
+    }
 }
 
 // Get references to the #generate element
